47cacd458f35083863de75f6ffa3fd486711fba7
Patrick d'Sá <patrickdesainfo@gmail.com>
2025-06-13 10:53:47 +0100

    Improving the menu and starting to create levels

--- code/Level.py
+++ code/Level.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 
 class Level:
-    def __init__(self):
-        self.window = None
-        self.name = None
+    def __init__(self, window, name, game_mode):
+        self.window = window
+        self.name = name
+        self.game_mode = game_mode # Modo de Jogo
         self.entity_list = None
 
     def run(self, ):
